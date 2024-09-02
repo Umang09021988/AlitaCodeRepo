37ad65740ab9db26f005341f54c07576f4e7482d
UmangH <56993340+Umang09021988@users.noreply.github.com>
2024-09-02 10:39:45 +0530

    Create src/test/java/com/example/DemoTest.java

---
+++ src/test/java/com/example/DemoTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoTest {
+
+    @Test
+    public void testMain() {
+        assertEquals(1, 1, "The test should pass with equal values.");
+    }
+}
