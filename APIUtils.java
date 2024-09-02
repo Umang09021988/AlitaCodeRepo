e4c8e41d193e89a548829edb0727f359066fffdd
UmangH <56993340+Umang09021988@users.noreply.github.com>
2024-09-02 13:40:55 +0530

    Create APIUtils.java

---
+++ APIUtils.java
@@ -0,0 +1,13 @@
+package com.example.utilities;
+
+public class APIUtils {
+
+    public static void sendRequest(String url) {
+        // Add request sending logic here
+    }
+
+    public static String getResponse() {
+        // Add response handling logic here
+        return "response";
+    }
+}
